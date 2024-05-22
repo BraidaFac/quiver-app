29910cd7e217601e0bf253ea0688a127a102defb
BraidaFac <facundobraida98@gmail.com>
2024-05-22 09:46:49 -0300

    netlify dont work

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,6 +1,7 @@
 import * as SDCCore from 'scandit-web-datacapture-core';
 import * as SDCBarcode from 'scandit-web-datacapture-barcode';
 import { barcodeCaptureLoader } from 'scandit-web-datacapture-barcode';
+import { PUBLIC_SCANDIT_KEY } from '$env/static/public';
 export async function initScanner() {
 	const view = new SDCCore.DataCaptureView();
 	const settings = new SDCBarcode.BarcodeCaptureSettings();
@@ -9,8 +10,7 @@ export async function initScanner() {
 	view.showProgressBar();
 	view.setProgressBarMessage('Loading ...');
 	await SDCCore.configure({
-		licenseKey:
-			'ASwUSSvQD793ByfXqSryjtE6gco/NY/DrFQCWvx5IGkYfUex31g8kRR/0qktUwDHVG7usOwTFt9gT1CbnnBBw4BeeFMDA9Ht2xZLRxo2iv+UE7WqL0Zv1mnOyKhFeueHCXyWo7bcGJEUOjrKmOhGpmRPUPgMUl4+JYEq9eDS7UygjA/IxFRNP4BZlZ/gW/ZWWGA7Zg5lX0NboM2btk4LQskjKfDz/ubn89B87x/YXIK6kKA8Ph3ikwPoboVKLMA4jCElhEU35pa9p7l34OECS/nRWgWfS5D5tx8gDrGXrtIfmzMZaIq8QFIaaeWXfcdzvuDTuo0zToArwBnc3b0+DyXGSdsA6D1JibAI3SYvOKbQ2fZEqC6GlnMDayvv03Bosvv3WW/OTHI5KtB0KhTCNSujHgsi29d8FCGcHZM/bDtNh95+jnQI3xyzOfpchQCl/LJFHyu/TO85DffwsF5zQQLPvNpN3FU+HniYlnySw2GT94xWhFj7Aiu3e0EIQUMUmEK2OXfXNWTNbTM81G/Rf3YSWKgtzi0AtZCmDZnGSIc5ragjTX6oSRwlupiyRnQtG+DNkiBk+YACoC5mLg9yU9Sma1WrGoDUxQNY2XSSHGcnp6Ly4tMd32B+K33v2ycumNuB7W44Idj9PuK4XH5LKdcA7t4zvrGWEd9w5lAMip9LBtg5JIOmpE4zn5SD7tOHkmlGQJF+n9l4oR8UXa2GchfF7mSGdZDG6RRJ+oyikAodz+8+MXa+Jvfgnnh0mq9yGCqH2+PAJdeVcjjlvU/r0UdTf/4CZoRJ',
+		licenseKey: PUBLIC_SCANDIT_KEY,
 		libraryLocation: 'https://unpkg.com/scandit-web-datacapture-barcode@6.23/build/engine',
 		moduleLoaders: [barcodeCaptureLoader()]
 	});
